2e7eb249a6e804df970a217c2427a8594d78beec
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-09-09 18:40:51 +0500

    database yaratildi

---
+++ src/db/database.sql
@@ -0,0 +1,4 @@
+create table todo(
+  todo_id SERIAL PRIMARY KEY,
+  description VARCHAR(255)
+);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 
 app.listen(2000, () => console.log('Server is running on port 2000'))
