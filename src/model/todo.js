2582051e3c020d66ff65a7fd383f552c1ab90924
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-09-10 17:57:44 +0500

    todo o'chirish va tahrirlash to'liq yozildi

--- src/model/todo.js
+++ src/model/todo.js
@@ -10,7 +10,7 @@ Todo.init(
       autoIncrement: true,
       primaryKey: true,
     },
-    descriptions: {
+    description: {
       type: DataTypes.STRING,
     },
   },
--- src/routers/todo.routes.js
+++ src/routers/todo.routes.js
@@ -3,13 +3,11 @@ import { Todo } from '../model/todo.js'
 
 export const todoRouter = Router()
 
-todoRouter.get('/todo', async (req, res) => {
+todoRouter.get('/todos', async (req, res) => {
   try {
     const todos = await Todo.findAll()
 
-    res.status(200).json({
-      todos,
-    })
+    res.status(200).json(todos || { msg: 'Todos not found' })
   } catch (error) {
     console.log(error.message)
   }
@@ -17,12 +15,17 @@ todoRouter.get('/todo', async (req, res) => {
 
 todoRouter.get('/todo/:id', async (req, res) => {
   try {
-    const { id } = req.params.id
+    const { id } = req.params
+
     const todo = await Todo.findOne({ where: { todo_id: id } })
 
-    res.status(200).json({
-      todo,
-    })
+    if (!todo) {
+      return res.status(404).json({
+        msg: 'Todo is not defined',
+      })
+    }
+
+    res.status(200).json(todo)
   } catch (error) {
     console.log(error.message)
   }
@@ -32,13 +35,55 @@ todoRouter.post('/todo', async (req, res) => {
   try {
     const { description } = req.body
 
+    if (!req.body || !description) {
+      return res.status(400).json({ msg: 'Please input fully' })
+    }
+
     const newTodo = await Todo.create({
       description: description,
     })
 
     res.status(201).json({
-      newTodo,
+      new_todo: newTodo,
       msg: 'successfully created',
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+})
+
+todoRouter.put('/todoUpdate/:id', async (req, res) => {
+  try {
+    const { description } = req.body
+    const { id } = req.params
+
+    if (!req.body || !description) {
+      return res.status(400).json({ msg: 'Please input fully' })
+    }
+
+    const updatedTodo = await Todo.update(
+      {
+        description: description,
+      },
+      { where: { todo_id: id } }
+    )
+
+    res.status(201).json({
+      msg: 'successfully created',
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+})
+
+todoRouter.delete('/todoDel/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const updatedTodo = await Todo.destroy({ where: { todo_id: id } })
+
+    res.status(201).json({
+      msg: 'successfully deleted',
     })
   } catch (error) {
     console.log(error.message)
